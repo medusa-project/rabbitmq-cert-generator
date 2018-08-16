49721bef58e721635b80f3082fdcba78b8ee26b9
Howard Ding <hding2@illinois.edu>
2018-08-16 10:08:29 -0500

    Add generator.

---
+++ generate.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+#This is from www.rabbitmq.com/ssl.html,
+#almost directly. Run it and it will generate
+#the certs you see there (as of 2108-08-16).
+
+if [[ -z $1 ]]; then
+    echo "USAGE: $0 <random_password>"
+    exit 1
+fi
+
+PASSWORD="$1"
+
+mkdir -p testca
+cd testca
+mkdir -p certs private
+chmod 700 private
+echo 01 > serial
+touch index.txt
+
+cat > openssl.cnf <<'EOF'
+[ ca ]
+default_ca = testca
+
+[ testca ]
+dir = .
+certificate = $dir/cacert.pem
+database = $dir/index.txt
+new_certs_dir = $dir/certs
+private_key = $dir/private/cakey.pem
+serial = $dir/serial
+
+default_crl_days = 7
+default_days = 365
+default_md = sha256
+
+policy = testca_policy
+x509_extensions = certificate_extensions
+
+[ testca_policy ]
+commonName = supplied
+stateOrProvinceName = optional
+countryName = optional
+emailAddress = optional
+organizationName = optional
+organizationalUnitName = optional
+domainComponent = optional
+
+[ certificate_extensions ]
+basicConstraints = CA:false
+
+[ req ]
+default_bits = 2048
+default_keyfile = ./private/cakey.pem
+default_md = sha256
+prompt = yes
+distinguished_name = root_ca_distinguished_name
+x509_extensions = root_ca_extensions
+
+[ root_ca_distinguished_name ]
+commonName = hostname
+
+[ root_ca_extensions ]
+basicConstraints = CA:true
+keyUsage = keyCertSign, cRLSign
+
+[ client_ca_extensions ]
+basicConstraints = CA:false
+keyUsage = digitalSignature,keyEncipherment
+extendedKeyUsage = 1.3.6.1.5.5.7.3.2
+
+[ server_ca_extensions ]
+basicConstraints = CA:false
+keyUsage = digitalSignature,keyEncipherment
+extendedKeyUsage = 1.3.6.1.5.5.7.3.1
+
+EOF
+
+openssl req -x509 -config openssl.cnf -newkey rsa:2048 -days 365 \
+    -out cacert.pem -outform PEM -subj /CN=MyTestCA/ -nodes
+openssl x509 -in cacert.pem -out cacert.cer -outform DER
+
+cd ..
+
+mkdir -p server
+cd server
+openssl genrsa -out key.pem 2048
+openssl req -new -key key.pem -out req.pem -outform PEM \
+    -subj /CN=$(hostname)/O=server/ -nodes
+cd ../testca
+openssl ca -config openssl.cnf -in ../server/req.pem -out \
+    ../server/cert.pem -notext -batch -extensions server_ca_extensions
+cd ../server
+openssl pkcs12 -export -out keycert.p12 -in cert.pem -inkey key.pem \
+    -passout pass:$PASSWORD
+
+cd ..
+mkdir -p client
+cd client
+openssl genrsa -out key.pem 2048
+openssl req -new -key key.pem -out req.pem -outform PEM \
+    -subj /CN=$(hostname)/O=client/ -nodes
+cd ../testca
+openssl ca -config openssl.cnf -in ../client/req.pem -out \
+    ../client/cert.pem -notext -batch -extensions client_ca_extensions
+cd ../client
+openssl pkcs12 -export -out keycert.p12 -in cert.pem -inkey key.pem \
+    -passout pass:$PASSWORD
+
